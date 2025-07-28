8df9df7436a6bd8249c781f6e50661875142f152
AbhishekN843 <abhu843@gamil.com>
2025-07-28 15:56:14 +0530

    added new file

--- varDeclartion.js
+++ varDeclartion.js
@@ -1,15 +1,15 @@
-console.log("hello from nodejs")
+console.log("hello from nodejs");
 // var let const
 
-var a =10
+var a = 10;
 {
-    console.log("Accecing inside the a lock",a)
-    var b =20
-    console.log("Accessing b iside block",b )
+  console.log("Accecing inside the a lock", a);
+  var b = 20;
+  console.log("Accessing b iside block", b);
 }
-console.log("Accessing b outide the block",b)
+console.log("Accessing b outide the block", b);
 
-// git adding 
+// git adding
 // git add varDeclartion
 // git commit -m 'adding to git'
 // gti push origin main
